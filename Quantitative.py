07e7342fe1fe1894cb9d01ded02acb7d02ef5269
Uttam <126519215+studyuttam@users.noreply.github.com>
2024-06-22 00:48:22 +0100

    Update_Quantitative

--- Quantitative.py
+++ Quantitative.py
@@ -3,121 +3,156 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit_shadcn_ui as ui
+import uk_map
 
 
 #import excel file name disabilitycensus2021.xlsx sheet name Table 6
 
 df = pd.read_excel('Data/disabilitycensus2021.xlsx', sheet_name='Table 6')
 
-st.write(df.head())
-
-#make multi select filters for local authority and age group on the side bar
-local_authority = st.sidebar.multiselect('Local Authority', df['Local Authority'].unique())
-age_group = st.sidebar.multiselect('Age Group', df['Age'].unique())
-
-#write code if nothing is selected for local_authority and age_group then display a message to select local_authority and age_group for the data to be displayed
-if len(local_authority) == 0 or len(age_group) == 0:
-    st.write('Please select Local Authority and Age Group')
-
-#display the selected local authority and age group
-st.write('Local Authority:', local_authority)
-st.write('Age Group:', age_group)
+st.title('Quantitative Analysis')
 
 
-#st_card('Completed Orders', value=76.4, show_progress=True)
+tab1, tab2, tab3, tab4 = st.tabs(['Census 2021', 'SEN Dashboard', 'Employment', 'Education'])
+with tab1:
+  st.header('Census 2021')
+  # Create two columns for the multiselects
+  col1, col2 = st.columns(2)
 
-#display df['Count'] for selected local authority and age group in card
-
-#filter the data based on the selected local authority and age group
-disabled_population = df[(df['Local Authority'].isin(local_authority))
-                    & (df['Age'].isin(age_group))
-                    & (df['Category'] == 'Four category')
-                    & (df['Sex'].isin(['Male','Female']))
-                    & (df['Disability Status'].isin(['Disabled; limited a lot','Disabled; limited a little']))]
-
-st.write('Disabled Population:', disabled_population['Count'].sum())
-
-# Total population
-
-total_population = df[(df['Local Authority'].isin(local_authority))
-                    & (df['Age'].isin(age_group))
-                    & (df['Category'] == 'Four category')
-                    & (df['Sex'].isin(['Male','Female']))
-                    & (df['Disability Status'].isin(['Disabled; limited a lot']))]
-
-st.write('Total Population:', total_population['Population'].sum())
+  # Place each multiselect in one column
+  with col1:
+    local_authority = st.multiselect('Local Authority', df['Local Authority'].unique(), default=['Brent'])
+  with col2:
+    age_group = st.multiselect('Age Group', df['Age'].unique(), default=['15 to 19'])
 
 
-#display total population and disabled population in bar chart
-plt.figure(figsize=(10, 5))
-sns.barplot(x=['Total Population', 'Disabled Population'], y=[total_population['Population'].sum(), disabled_population['Count'].sum()])
-plt.title('Total Population vs Disabled Population')
-st.pyplot(plt)
+  #write code if nothing is selected for local_authority and age_group then display a message to select local_authority and age_group for the data to be displayed
+  if len(local_authority) == 0 or len(age_group) == 0:
+      st.write('Please select Local Authority and Age Group')
+  else:
+
+    #display the selected local authority and age group
+    st.write('Local Authority:', local_authority)
+    st.write('Age Group:', age_group)
 
 
-#display a pie chart for different sex in the disabled
-plt.figure(figsize=(10, 5))
-disabled_population.groupby('Sex')['Count'].sum().plot.pie(autopct='%1.1f%%')
-plt.title('Disabled Population by Sex')
-st.pyplot(plt)
+    #st_card('Completed Orders', value=76.4, show_progress=True)
 
-st.write('Disabled Population:', disabled_population.groupby('Sex')['Count'].sum())
+    #display df['Count'] for selected local authority and age group in card
+
+    #filter the data based on the selected local authority and age group
+    disabled_population = df[(df['Local Authority'].isin(local_authority))
+                        & (df['Age'].isin(age_group))
+                        & (df['Category'] == 'Four category')
+                        & (df['Sex'].isin(['Male','Female']))
+                        & (df['Disability Status'].isin(['Disabled; limited a lot','Disabled; limited a little']))]
+
+    st.write('Disabled Population:', disabled_population['Count'].sum())
+
+    # Total population
+
+    total_population = df[(df['Local Authority'].isin(local_authority))
+                        & (df['Age'].isin(age_group))
+                        & (df['Category'] == 'Four category')
+                        & (df['Sex'].isin(['Male','Female']))
+                        & (df['Disability Status'].isin(['Disabled; limited a lot']))]
+
+    st.write('Total Population:', total_population['Population'].sum())
+
+    #Total Population and Disabled Population in a card
+    cols = st.columns(2)
+    with cols[0]:
+      ui.metric_card(title="Total Population", content=total_population['Population'].sum(), description=" ", key="card1")
+    with cols[1]:
+      ui.metric_card(title="Disabled Population", content=disabled_population['Count'].sum(), description=" ", key="card2")
+    
+    
+    # Create a figure for 2x2 subplots
+    fig, axs = plt.subplots(2, 2, figsize=(25, 20)) 
+
+    #display total population and disabled population in bar chart
+    
+    sns.barplot(x=['Total Population', 'Disabled Population'], y=[total_population['Population'].sum(), disabled_population['Count'].sum()], ax=axs[0, 0])
+    axs[0,0].set_title('Total Population vs Disabled Population')
+    
+
+    # Plot 2: Disabled Population by Sex
+
+    #display a pie chart for different sex in the disabled
+    
+    disabled_population.groupby('Sex')['Count'].sum().plot.pie(autopct='%1.1f%%', ax=axs[0, 1])
+    axs[0, 1].set_title('Disabled Population by Sex')
+    
+    # Plot 3: Population Group by Disability Status
+    st.write('Disabled Population:', disabled_population.groupby('Sex')['Count'].sum())
+    poulation_grpby_category = df[(df['Local Authority'].isin(local_authority))
+                        & (df['Age'].isin(age_group))
+                        & (df['Category'] == 'Four category')
+                        & (df['Sex'].isin(['Male','Female']))
+                        ]
+
+    # Prepare data for seaborn
+    data = poulation_grpby_category.groupby('Disability Status')['Count'].sum().reset_index()
+
+    # Create a bar plot
+    
+    barplot = sns.barplot(x='Disability Status', y='Count', data=data, palette='viridis',ax=axs[1, 0])
+
+    # Add data labels
+    for bar in barplot.patches:
+        barplot.text(x = bar.get_x() + bar.get_width() / 2, 
+                    y = bar.get_height(), 
+                    s = f'{int(bar.get_height())}', 
+                    ha = 'center', 
+                    va = 'bottom')
+
+    # Set y-axis limit
+    axs[1,0].set_ylim(0, data['Count'].max() * 1.1)
+
+    axs[1, 0].tick_params(axis='x', rotation=45)
+    axs[1, 0].set_title('Population Group by Disability Status', fontsize=16, fontweight='bold', y=1.05)
+    
+    # Since there's no fourth plot specified, you can hide the fourth subplot or use it for another plot
+    axs[1, 1].axis('off')
+
+    # Adjust layout
+    plt.tight_layout()
+
+    # Display the figure in Streamlit
+    st.pyplot(fig)
+
+    #display disabled population of Top 10 local authorities in a bar chart
+
+    # Convert 'Count' column to numeric
+    df['Count'] = pd.to_numeric(df['Count'], errors='coerce')
+    uk_disabled_population = df[(df['Age'].isin(age_group))
+                        & (df['Category'] == 'Four category')
+                        & (df['Sex'].isin(['Male','Female']))
+                        & (df['Disability Status'].isin(['Disabled; limited a lot','Disabled; limited a little']))]
+
+    # Get the top 10 local authorities by disabled population
+    disabled_population_top10 = uk_disabled_population.groupby('Local Authority')['Count'].sum().nlargest(10)
+
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=disabled_population_top10.index, y=disabled_population_top10.values)
+    plt.xticks(rotation=45)
+    plt.title('Disabled Population For Top 10 Local Authority')
+    st.pyplot(plt)
 
 
-poulation_grpby_category = df[(df['Local Authority'].isin(local_authority))
-                    & (df['Age'].isin(age_group))
-                    & (df['Category'] == 'Four category')
-                    & (df['Sex'].isin(['Male','Female']))
-                    ]
+    #UK Map
+    st.map()
 
-import seaborn as sns
+with tab2:
+  st.header('SEN Dashboard')
+  st.write('Coming Soon')
 
-# Prepare data for seaborn
-data = poulation_grpby_category.groupby('Disability Status')['Count'].sum().reset_index()
+with tab3:
+  st.header('Employment')
+  st.write('Coming Soon')
 
-# Create a bar plot
-plt.figure(figsize=(10, 5))
-barplot = sns.barplot(x='Disability Status', y='Count', data=data, palette='viridis')
+with tab4:
+  st.header('Education')
+  st.write('Coming Soon')
 
-# Add data labels
-for bar in barplot.patches:
-    barplot.text(x = bar.get_x() + bar.get_width() / 2, 
-                 y = bar.get_height(), 
-                 s = f'{int(bar.get_height())}', 
-                 ha = 'center', 
-                 va = 'bottom')
-
-# Set y-axis limit
-plt.ylim(0, data['Count'].max() * 1.1)
-
-plt.xticks(rotation=45)
-plt.title('Population Group by Disability Status', fontsize=16, fontweight='bold', y=1.05)
-plt.tight_layout(pad=1.0)
-st.pyplot(plt)
-
-#display disabled population of Top 10 local authorities in a bar chart
-
-# Convert 'Count' column to numeric
-df['Count'] = pd.to_numeric(df['Count'], errors='coerce')
-uk_disabled_population = df[(df['Age'].isin(age_group))
-                    & (df['Category'] == 'Four category')
-                    & (df['Sex'].isin(['Male','Female']))
-                    & (df['Disability Status'].isin(['Disabled; limited a lot','Disabled; limited a little']))]
-
-# Get the top 10 local authorities by disabled population
-disabled_population_top10 = uk_disabled_population.groupby('Local Authority')['Count'].sum().nlargest(10)
-
-plt.figure(figsize=(10, 5))
-sns.barplot(x=disabled_population_top10.index, y=disabled_population_top10.values)
-plt.xticks(rotation=45)
-plt.title('Disabled Population For Top 10 Local Authority')
-st.pyplot(plt)
-
-from streamlit_card import card
-
-hasClicked = card(
-  title="Hello World!",
-  text="Some description",
-  image="http://placekitten.com/200/300",
-  url="https://github.com/gamcoh/st-card"
-)
